1f3c730f822b14b2fb75c6574361dc6cadb46d72
Dubravko <dev.richard.npm98@gmail.com>
2023-11-10 10:40:06 +0700

    update code

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,24 @@ import * as WebSocket from 'ws';
 import * as SocketService from './services/socket.service'
 const express = require('express')
 
-// app.get('/', (req, res) => {
-//     res.send('Hey this is my API running 🥳')
-// })
+const app = express()
+const PORT = 8083
+
+const server = app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+    new Socket()
+})
 
 // app.get('/about', (req, res) => {
 //     res.send('This is my about route..... ')
 // })
 
 class Socket {
+
+    private wss = new WebSocket.Server({ server });
     private rooms = new Map();
     private roomsId = new Map();
 
@@ -19,23 +28,13 @@ class Socket {
     }
 
     public start() {
-        const app = express()
-        const PORT = 8083
-
-        const server = app.listen(PORT, () => {
-            console.log(`Server started on port ${PORT}`);
-        });
-
-        const wss = new WebSocket.Server({ server });
-
         const option = {
             rooms: this.rooms,
             roomsId: this.roomsId
         }
-        SocketService.start(wss, option)
+        SocketService.start(this.wss, option)
     }
 }
 
-new Socket()
-
 // Export the Express API
+module.exports = app
