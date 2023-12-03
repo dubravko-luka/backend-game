1a61c087f1fa141ea6fab3519d130934e547ba24
Dubravko <dev.richard.npm98@gmail.com>
2023-12-03 22:05:47 +0700

    udpate code

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import * as express from "express";
-// import * as cors from "cors";
+import * as cors from "cors";
 const dotenv = require('dotenv');
 
 dotenv.config();
@@ -15,19 +15,23 @@ class App {
   private config() {
     this.app.use(express.json({ limit: "10mb" }));
     this.app.use(express.urlencoded({ extended: false }));
-    // if (process.env.NODE_ENV !== "production") {
-    //   this.app.use(cors({
-    //     origin: [
-    //       '*'
-    //     ]
-    //   }));
-    // } else {
-    //   this.app.use(cors({
-    //     origin: [
-    //       '*'
-    //     ]
-    //   }));
-    // }
+    if (process.env.NODE_ENV !== "production") {
+      this.app.use(cors({
+        origin: [
+          'http://localhost:3000',
+          'http://localhost:3001',
+          'https://supernova.demo-website.click'
+        ]
+      }));
+    } else {
+      this.app.use(cors({
+        origin: [
+          'http://localhost:3000',
+          'http://localhost:3001',
+          'https://supernova.demo-website.click'
+        ]
+      }));
+    }
     this.app.use(express.static("src"));
     this.app.use((req, res, next) => {
       const err = new Error('Not Found')
