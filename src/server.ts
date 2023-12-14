318054047d4a1d9910124b3b1a541c2d04970bd8
Dubravko <dev.richard.npm98@gmail.com>
2023-12-14 11:03:12 +0700

    update wss

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,21 @@
-import app from './app';
-import * as http from 'http';
+// import app from './app';
+// import * as http from 'http';
 
-export const server = http.createServer(app);
+// export const server = https.createServer(app);
+
+// const port = process.env.PORT || 8083
+
+// server.listen(port, () => {
+//     console.log(`Server started on port ${port}`);
+// });
+
+import * as https from 'https';
+import * as fs from 'fs';
+
+const server = https.createServer({
+    key: fs.readFileSync('./ssl/key.pem'),
+    cert: fs.readFileSync('./ssl/cert.pem')
+});
 
 const port = process.env.PORT || 8083
 
