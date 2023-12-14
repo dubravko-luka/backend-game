cefd8f509078025513c4fef2e234aaa7fa9a4147
Dubravko <dev.richard.npm98@gmail.com>
2023-12-14 11:27:18 +0700

    update wss

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,26 @@
-import app from './app';
-import * as http from 'http';
+// import app from './app';
+// import * as http from 'http';
 
-export const server = https.createServer(app);
-
-const port = process.env.PORT || 8083
-
-server.listen(port, () => {
-    console.log(`Server started on port ${port}`);
-});
-
-// import * as https from 'https';
-// import * as fs from 'fs';
-// import * as path from 'path';
-
-// export const server = https.createServer({
-//     key: fs.readFileSync(path.join(__dirname, './ssl/key.pem'), 'utf-8'),
-//     cert: fs.readFileSync(path.join(__dirname, './ssl/cert.pem'), 'utf-8')
-// });
+// export const server = http.createServer(app);
 
 // const port = process.env.PORT || 8083
 
 // server.listen(port, () => {
 //     console.log(`Server started on port ${port}`);
 // });
+
+import * as https from 'https';
+import * as fs from 'fs';
+import * as path from 'path';
+import app from './app';
+
+export const server = https.createServer({
+    key: fs.readFileSync(path.join(__dirname, './ssl/key.pem'), 'utf-8'),
+    cert: fs.readFileSync(path.join(__dirname, './ssl/cert.pem'), 'utf-8')
+}, app);
+
+const port = process.env.PORT || 8083
+
+server.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
