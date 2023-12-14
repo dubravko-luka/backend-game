1adcb83e32c6c6704d314d51f7744299d9392cae
Dubravko <dev.richard.npm98@gmail.com>
2023-12-14 11:10:57 +0700

    update wss

--- src/server.ts
+++ src/server.ts
@@ -11,10 +11,11 @@
 
 import * as https from 'https';
 import * as fs from 'fs';
+import * as path from 'path';
 
-const server = https.createServer({
-    key: fs.readFileSync('./ssl/key.pem'),
-    cert: fs.readFileSync('./ssl/cert.pem')
+export const server = https.createServer({
+    key: fs.readFileSync(path.join(__dirname, './ssl/key.pem'), 'utf-8'),
+    cert: fs.readFileSync(path.join(__dirname, './ssl/cert.pem'), 'utf-8')
 });
 
 const port = process.env.PORT || 8083
